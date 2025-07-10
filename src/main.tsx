78b2da23731246314b3eada2732a69e6fdfc757e
shiftregister-vg <shiftregister@protonmail.com>
2025-07-10 14:28:56 -0700

    Started tweaking to add logseq stuff

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,31 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import logseq from '@logseq/libs'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const main = async () => {
+  logseq.App.onMacroRendererSlotted(
+    async ({ slot, payload: { uuid, arguments: args } }) => {
+      const [type] = args
+      if (!type || !type.startsWith(':odyssey_')) { return }
+
+      const odysseyId = `odyssey_${uuid}_${slot}`
+
+      logseq.provideUI({
+        key: odysseyId,
+        slot,
+        reset: true,
+        template: `<div id=${odysseyId}></div>`,
+      })
+
+      setTimeout(() => {
+        createRoot(parent.document.querySelector(`#${odysseyId}`)!).render(
+          <StrictMode>
+            <App />
+          </StrictMode>,
+        )
+      }, 0)
+    })
+}
+
+logseq.ready(main).catch(console.error)
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc'
+import logseqDevPlugin from 'vite-plugin-logseq'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), logseqDevPlugin],
 })
