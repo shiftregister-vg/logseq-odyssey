3ed00133c6fff83e67047a37373a40a6cd837024
shiftregister-vg <shiftregister@protonmail.com>
2025-07-25 11:33:44 -0700

    fix: load wasm from relative path

--- src/lib/wasm-loader.ts
+++ src/lib/wasm-loader.ts
@@ -2,15 +2,10 @@ import '@logseq/libs';
 declare const logseq: any;
 
 export async function loadWasm() {
-  if (!(logseq.settings.wasmUrl)) {
-    console.error("WASM module URL not found in settings.");
-    return;
-  }
-
   const go = new (window as any).Go();
   try {
     const result = await WebAssembly.instantiateStreaming(
-      fetch(logseq.settings.wasmUrl),
+      fetch('./odyssey.wasm'),
       go.importObject
     );
     go.run(result.instance);
